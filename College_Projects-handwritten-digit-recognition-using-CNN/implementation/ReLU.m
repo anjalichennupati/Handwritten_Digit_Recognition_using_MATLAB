7ec62ba97f6ddde6a0ae18e348d020044db99bdf
Anjali Chennupati <anjali.m.chennupati@gmail.com>
2025-02-24 22:07:44 +0530

    Delete College_Projects-handwritten-digit-recognition-using-CNN/implementation directory

--- College_Projects-handwritten-digit-recognition-using-CNN/implementation/ReLU.m
+++
@@ -1,8 +0,0 @@
-function out = ReLU(conv)
-    % ReLU - Rectified Linear unit - introduces non linearity into a neural
-    % network
-    % conv - complex valued variable
-    % ReLU is applied to the real and imaginary part of conv separately
-
-    out = max(0,real(conv)) + 1i*(max(0, imag(conv)));
-end
--- College_Projects-handwritten-digit-recognition-using-CNN/implementation/rng.m
+++
@@ -1,5 +0,0 @@
-function rng(x)
-    randn('seed',x) % generates randomly dist from an std normal dist
-    rand('seed',x) % generates randomly from a unif dist 
-
-end
